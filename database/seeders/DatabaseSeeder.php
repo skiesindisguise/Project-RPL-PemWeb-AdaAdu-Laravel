b78a7a2356b8cc8df6e76ceadfa291c7b8fe597a
skiesindisguise <khansaamani101@gmail.com>
2024-06-24 13:21:13 +0700

    memperbaiki error <?

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,45 +1,14 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+namespace Database\Seeders;
 
-class CreateReportsTable extends Migration
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function run(): void
     {
-        Schema::create('reports', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); //FK ke user
-            $table->string('title');
-            $table->string('tag');
-            $table->boolean('anonymous');
-            $table->boolean('public');
-            $table->text('description');
-            $table->string('author');
-            $table->date('report_date');
-            $table->integer('votes')->default(0);
-            $table->string('status');
-            $table->string('status_desc')->nullable();
-            $table->string('photo')->nullable();
-            $table->timestamps();
-
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('reports');
+        $this->call(UsersSeeder::class);
+        $this->call(ReportSeeder::class);
     }
 }
