4639b6126e8ea62647ac2dea59a81468fb1aa187
fqhkhrzm <muhammadfaqihk16@gmail.com>
2024-07-01 01:50:35 +0700

    menambahkan beberapa animasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('style/index.css') }}">
+    <script src="https://unpkg.com/scrollreveal"></script>
 </head>
 
 <body>
@@ -164,6 +165,10 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    <script>
+        ScrollReveal().reveal('.main-section', { delay: 1000 });
+        ScrollReveal().reveal('.bg-main-content', { delay: 1000 });
+        ScrollReveal().reveal('.testimonials-section', { delay: 1000 });
+    </script>
 </body>
 </html>
